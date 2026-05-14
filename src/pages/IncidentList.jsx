2c66e035a57b2f20f97abab23f18e62fee46b985
AbbasovRasim <ras.abbasov@gmail.com>
2026-05-15 00:43:25 +0400

    secure endpoints

--- src/pages/IncidentList.jsx
+++ src/pages/IncidentList.jsx
@@ -37,7 +37,7 @@ const StatusBadge = ({ status }) => {
         padding: "0.7rem 1.3rem", // daha böyük
         fontSize: "0.5rem", // daha böyük
         borderRadius: "40px", // daha yumrus
-        fontWeight: "500",
+        fontWeight: "600",
         backgroundColor: isPending ? "#fff3cd" : "#d1e7dd",
         color: isPending ? "#856404" : "#0f5132",
       }}
