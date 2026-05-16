8a3da80c242f6470bd06239ba761a05d25826b9f
AbbasovRasim <ras.abbasov@gmail.com>
2026-05-16 19:57:19 +0400

    Temporary: open register to everyone

--- src/App.jsx
+++ src/App.jsx
@@ -55,17 +55,8 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
 
-          {/* ✅ REGISTER YALNIZ ADMIN ÜÇÜN */}
-          <Route
-            path="/register"
-            element={
-              localStorage.getItem("role") === "ADMIN" ? (
-                <Register />
-              ) : (
-                <Navigate to="/login" />
-              )
-            }
-          />
+          {/* ✅ MÜVƏQQƏTİ - HƏR KƏSƏ AÇIQ */}
+          <Route path="/register" element={<Register />} />
 
           <Route element={<PrivateRoute />}>
             <Route element={<Navbar />}>
